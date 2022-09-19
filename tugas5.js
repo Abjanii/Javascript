5666f79896bcffb246eae3ea3a4e6a5190f09c22
Icappucino <93023359+Abjanii@users.noreply.github.com>
2022-09-19 22:46:22 +0700

    tugas 5 JavaScript

---
+++ tugas5.js
@@ -0,0 +1,34 @@
+const hitung = (tombol) => {
+    //tangkap id form
+    let frm = document.getElementById("calcForm");
+    let a1 = parseFloat(frm.angka1.value);
+    let a2 = parseFloat(frm.angka2.value);
+
+    let total = "";
+    if (isNaN(a1) || isNaN(a2)) {
+      alert("Harap masukkan angka!!");
+    } else {
+      switch (tombol) {
+        case "btn_tambah":
+          total = a1 + a2; //rumus
+          frm.hasil.value = total; //penempatan hasil
+          break;
+        case "btn_kurang":
+          total = a1 - a2; //rumus
+          frm.hasil.value = total; //penempatan hasil
+          break;
+        case "btn_kali":
+          total = a1 * a2; //rumus
+          frm.hasil.value = total; //penempatan hasil
+          break;
+        case "btn_bagi":
+          total = a1 / a2; //rumus
+          frm.hasil.value = total; //penempatan hasil
+          break;
+        case "btn_pangkat":
+          total = Math.pow(a1, a2); //rumus
+          frm.hasil.value = total; //penempatan hasil
+          break;
+      }
+    }
+  };
